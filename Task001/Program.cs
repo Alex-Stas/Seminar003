4a14e9d7e0bacc53c0e28cb921f0c8db0b3d8da9
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-18 12:11:54 +0900

    Entering two numbers

---
+++ Task001/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] InputSomeNumbers()
+{
+    int[] twoNums = new int[2];
+    Console.Write("Enter first number: ");
+    twoNums[0] = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter second number: ");
+    twoNums[1] = int.Parse(Console.ReadLine()!);
+    return twoNums;
+}
+
+int[] array = InputSomeNumbers();
+Console.WriteLine($"First {array[0]}, second {array[1]}");
+
