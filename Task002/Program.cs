ae49dcb3477fad98f1bfd0325512ac439a16fdbe
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-18 14:09:09 +0900

    Quadrants

---
+++ Task002/Program.cs
@@ -0,0 +1,24 @@
+﻿int InputNum (string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+int Quart (int x, int y)
+{
+if (x > 0 && y > 0) return 1;
+else if (x < 0 && y > 0) return 2;
+else if (x < 0 && y < 0) return 3;
+else if (x > 0 && y < 0) return 4;
+else return -1;
+}
+
+int x = InputNum("Введите координату х: ");
+int y = InputNum("Введите координату y: ");
+
+int res = Quart(x, y);
+
+if (res == - 1)
+Console.WriteLine("Точка находится на пересечении четвертей");
+else
+Console.WriteLine($"Четверть {res}");
