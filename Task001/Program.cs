3e4de887755e3f9880583cb2688c5233b0cd3052
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-18 13:55:47 +0900

    With third digit via array

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,37 +1,78 @@
-﻿int[] InputSomeNumbers()
+﻿// ************************************************
+// Поиск третьего числа через массив
+
+void PrintArray(int[] array)
 {
-    Console.Write("Enter quantity of numbers: ");
-    int size = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i]);
+if (i < array.Length - 1)
+Console.Write(", ");
+else
+Console.Write(".");
+}
+}
 
-    int[] array = new int[size];
+int num = int.Parse(Console.ReadLine());
+int temp = num;
 
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"Enter {i + 1} number: ");
-        array[i] = int.Parse(Console.ReadLine()!);
+int count = 0;
+while (temp != 0)
+{
+temp = temp / 10; // num /= 10;
+count++;
+}
+
+int[] array = new int[count];
+
+for (int i = array.Length - 1; i >= 0; i--) // в обратном напралении поскольку деление дает остаток с конца
+{
+array[i] = num % 10;
+num /= 10; // аналог num = num / 10
+}
+
+PrintArray(array);
+
+Console.Write($"\n{array[2]}");
+
+
+// **************************************************************************
+// Ввод любого количества переменных.
+
+// int[] InputSomeNumbers()
+// {
+//     Console.Write("Enter quantity of numbers: ");
+//     int size = int.Parse(Console.ReadLine()!);
+
+//     int[] array = new int[size];
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"Enter {i + 1} number: ");
+//         array[i] = int.Parse(Console.ReadLine()!);
 
 
 
 
-    }
+//     }
         
-    return array;
-}
+//     return array;
+// }
 
-void PrintArray (int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i]);
-        if (i < array.Length - 1)
-            Console.Write(", ");
-        else
-            Console.Write("."); 
-    }
-}
+// void PrintArray (int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i]);
+//         if (i < array.Length - 1)
+//             Console.Write(", ");
+//         else
+//             Console.Write("."); 
+//     }
+// }
 
-int[] myArray = InputSomeNumbers();
-PrintArray(myArray);
+// int[] myArray = InputSomeNumbers();
+// PrintArray(myArray);
 
 
 // ******************************************************
